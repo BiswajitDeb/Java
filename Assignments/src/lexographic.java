b55435c3d94b288fa13c224c37fa53bc0fa110ca
Biswajit Deb <biswa2015.bd@gmail.com>
2021-11-12 17:20:30 +0530

    Assignment 2 update

---
+++ Assignments/src/lexographic.java
@@ -0,0 +1,56 @@
+// A O(n) solution for finding rank of string
+
+import java.util.Scanner;
+
+class loxographicrank
+{
+    static int fact(int n)
+    {
+        return (n <= 1) ? 1 : n * fact(n - 1);
+    }
+
+    static void count_update(int[] count, char[] str)
+    {
+        int i;
+
+        for (i = 0; i < str.length; ++i)
+            ++count[str[i]];
+
+        for (i = 1; i < 200; ++i)
+            count[i] += count[i - 1];
+    }
+
+    static void updatecount(int[] count, char ch)
+    {
+        int i;
+        for (i = ch; i < 200; ++i)
+            --count[i];
+    }
+
+    static int findRank(char[] str)
+    {
+        int len = str.length;
+        int mul = fact(len);
+        int rank = 1, i;
+
+        int count[] = new int[200];
+        count_update(count, str);
+
+        for (i = 0; i < len; ++i) {
+            mul /= len - i;
+            rank += count[str[i] - 1] * mul;
+            updatecount(count, str[i]);
+        }
+        return rank;
+    }
+
+    // Driver code
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        char str[] = sc.nextLine().toCharArray();
+        System.out.println("Lexographic rank for is "+findRank(str));
+    }
+}
+
--- Assignments/src/remove_b_ac.java
+++ Assignments/src/remove_b_ac.java
@@ -1,2 +1,40 @@
-public class remove_b_ac {
+import java.util.Scanner;
+class remove_b_ac
+{
+    static void stringFilter(char str[])
+    {
+        int n = str.length;
+        int i = -1;
+        int j = 0;
+
+        while (j < n)
+        {
+            if (j < n-1 && str[j] == 'a' && str[j+1] == 'c')
+                j += 2;
+
+            else if (str[j] == 'b')
+                j++;
+
+            else if (i >= 0 && str[j] == 'c' && str[i] == 'a')
+            {
+                i--;
+                j++;
+            }
+
+            else
+                str[++i] = str[j++];
+
+        }
+        System.out.println(new String(str).substring(0,i+1));
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println("Enter a string : ");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        stringFilter(str.toCharArray());
+
+
+    }
 }
